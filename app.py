0b5d25fa78083304615c9647aee56d253b6cb943
Virginia Wu <virginiawu@virginias-air.myfiosgateway.com>
2021-05-31 16:05:44 -0400

    Part1 Step5 - Profile Edit function added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -211,7 +211,32 @@ def stop_following(follow_id):
 @app.route('/users/profile', methods=["GET", "POST"])
 def profile():
     """Update profile for current user."""
-    
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+
+    form = UserEditForm(obj=g.user)
+    if form.validate_on_submit():
+        
+
+        user = User.authenticate(form.username.data,
+                                 form.password.data)
+
+        if user:
+            g.user.username = form.username.data
+            g.user.email = form.email.data
+            g.user.image_url = form.image_url.data
+            g.user.header_image_url = form.header_image_url.data
+            g.user.bio = form.bio.data
+
+            db.session.commit()
+            flash('User updated.', 'success')
+            return redirect(f'/users/{g.user.id}')
+
+        flash("Invalid credentials.", 'danger')
+            
+    return render_template('/users/edit.html',form=form, user=g.user)
+
     # IMPLEMENT THIS
 
 
