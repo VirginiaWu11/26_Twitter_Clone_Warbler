cd7f3147dd309370a159ea01a5c39437b1d950ac
Virginia Wu <virginiawu@virginias-air.myfiosgateway.com>
2021-06-11 23:37:40 -0400

    test message likes

--- test_message_model.py
+++ test_message_model.py
@@ -19,10 +19,12 @@ class UserModelTestCase(TestCase):
         """Create test client, add sample data."""
         db.drop_all()
         db.create_all()
-        u = User.signup("testuser3","test3@test.com","HASHED_PASSWORD3",None)
+        u1 = User.signup("testuser1","test1@test.com","HASHED_PASSWORD1",None)
+        u2 = User.signup("testuser2","test2@test.com","HASHED_PASSWORD2",None)
         db.session.commit()
 
-        self.u = User.query.get(u.id)
+        self.u1 = User.query.get(u1.id)
+        self.u2 = User.query.get(u2.id)
 
         self.client = app.test_client()
 
@@ -34,15 +36,37 @@ class UserModelTestCase(TestCase):
         
         m = Message(
             text="testingtext123",
-            user_id=self.u.id
+            user_id=self.u1.id
         )
 
         db.session.add(m)
         db.session.commit()
 
         # User should have 1 message
-        self.assertEqual(len(self.u.messages), 1)
-        self.assertEqual(self.u.messages[0].text, "testingtext123")
+        self.assertEqual(len(self.u1.messages), 1)
+        self.assertEqual(self.u1.messages[0].text, "testingtext123")
 
-   
+    def test_message_likes(self):
+        m1 = Message(
+            text="testingtext123",
+            user_id=self.u1.id
+        )
+
+        m2 = Message(
+            text="moreText",
+            user_id=self.u1.id
+        )
+
+        db.session.add_all([m1, m2])
+        db.session.commit()
+
+        self.u2.likes.append(m1)
+
+        db.session.commit()
+
+        likes = self.u2.likes
+        self.assertEqual(len(likes), 1)
+        self.assertEqual(likes[0].id, m1.id)
+
+
         
